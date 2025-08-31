305626ba8d3cdb827a5550a0aee48fda3705f11c
Nishant Gupta <gupta.nishant08042004@gmail.com>
2025-09-01 03:20:05 +0530

    contact us

--- learnex/components/FormInputs/TextInput.tsx
+++ learnex/components/FormInputs/TextInput.tsx
@@ -67,7 +67,7 @@ export default function TextInput({
             type={type}
             {...register(`${name}`, { required: true })}
             className={cn(
-              "block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-sm",
+              "block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6 text-sm",
               (errors[`${name}`] && "focus:ring-red-500 pl-8") ||
                 (icon && "pl-8")
             )}
---
+++ learnex/components/frontend/contact-us.tsx
@@ -0,0 +1,167 @@
+"use client";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import TextInput from "../FormInputs/TextInput";
+import PasswordInput from "../FormInputs/PasswordInput";
+import SubmitButton from "../FormInputs/SubmitButton";
+import { Mail, Send } from "lucide-react";
+import TextArea from "../FormInputs/TextAreaInput";
+
+export type RegisterInputProps = {
+  fullName: string;
+  email: string;
+  password: string;
+  phone: string;
+};
+
+const ContactUs: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<RegisterInputProps>();
+  async function onSubmit(data: RegisterInputProps) {
+    console.log(data);
+  }
+
+  return (
+    <section className="bg-gray-100 py-16 px-4">
+      <div className="max-w-6xl mx-auto">
+        <div className="flex items-center justify-center">
+          <div className="col-span-2 bg-white p-6 rounded-2xl shadow">
+            <h3 className="text-2xl text-center font-semibold mb-4">
+              Sign up to get your school onboard
+            </h3>
+            <form className="grid gap-4" onSubmit={handleSubmit(onSubmit)}>
+              <TextInput
+                label="Full Name"
+                register={register}
+                name="name"
+                errors={errors}
+                placeholder="John Doe"
+              />
+
+              <div className="grid md:grid-cols-2 gap-4">
+                <TextInput
+                  label="Email Address"
+                  register={register}
+                  name="email"
+                  type="email"
+                  errors={errors}
+                  placeholder="johndoe@gmail.com"
+                />
+                <TextInput
+                  label="Phone"
+                  register={register}
+                  name="phone"
+                  errors={errors}
+                  placeholder="+1234567890"
+                />
+              </div>
+
+              <div className="grid md:grid-cols-2 gap-4">
+                <TextInput
+                  label="School Name"
+                  register={register}
+                  name="school"
+                  errors={errors}
+                  placeholder="D.M.S.S.S."
+                />
+                <TextInput
+                  label="Country"
+                  register={register}
+                  name="country"
+                  errors={errors}
+                  placeholder="India"
+                />
+              </div>
+
+              <div className="grid md:grid-cols-2 gap-4">
+                <TextInput
+                  label="School Website/Social Media Website"
+                  register={register}
+                  name="schoolPage"
+                  errors={errors}
+                  placeholder="https://www.google.com/"
+                />
+                <TextInput
+                  label="No of Students"
+                  register={register}
+                  name="students"
+                  errors={errors}
+                  placeholder="100"
+                />
+              </div>
+
+              <div className="grid md:grid-cols-2 gap-4">
+                <TextInput
+                  label="Your Role"
+                  register={register}
+                  name="role"
+                  errors={errors}
+                  placeholder="Principal"
+                />
+                <TextInput
+                  label="Product Interest(Which features are you looking for?)"
+                  register={register}
+                  name="productInterest"
+                  errors={errors}
+                  placeholder="Premium"
+                />
+              </div>
+
+              <TextArea
+                label="Your Role"
+                register={register}
+                name="role"
+                errors={errors}
+              />
+              <TextInput
+                label="How did you hear about us?"
+                register={register}
+                name="referral"
+                errors={errors}
+                placeholder="Instagram"
+              />
+
+              <SubmitButton
+                title="Submit"
+                loading={isLoading}
+                loadingTitle="Sending please wait..."
+                buttonIcon={Send}
+              />
+            </form>
+          </div>
+        </div>
+        <div className="py-8 grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="bg-green-800 text-white p-6 rounded-2xl">
+            <h3 className="font-semibold text-xl mb-2">
+              Speak to someone in sales
+            </h3>
+            <p className="text-sm mb-4 py-4">
+              To create a more value-added solution, is essential to an analysis
+              of the possibilities of improvement.
+            </p>
+            <button className="bg-white text-green-800 px-4 py-2 rounded-full text-sm font-semibold hover:bg-gray-100 transition duration-300">
+              Book Appointment
+            </button>
+          </div>
+          <div className="bg-lime-400 p-6 rounded-2xl">
+            <h3 className="font-semibold mb-2 text-xl">Contact to our team</h3>
+            <p className="text-sm mb-4 py-4">
+              To create a more value-added solution, is essential to an analysis
+              of the possibilities of improvement.
+            </p>
+            <button className="bg-green-800 text-white px-4 py-2 rounded-full text-sm font-semibold hover:bg-green-700 transition duration-300">
+              Send a Mail
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContactUs;
